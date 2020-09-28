5477d225b72fb01aa924ed577f941101f745cb19
Monica <moniarbe233@gmail.com>
2020-09-28 17:13:31 -0500

    Componente Equipo

--- sportup/src/App.js
+++ sportup/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Routes } from './routers/Routes'
 
+
 export const App = () => {
     return (
+        
         <Routes></Routes>
+        
     )
 }
--- sportup/src/routers/Routes.jsx
+++ sportup/src/routers/Routes.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import { Inicio } from '../components/Inicio'
+import { Equipo } from '../components/Equipo'
 
 export const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route  path='/' component = { Inicio } />
-                <Route exact path='/Inicio' component = { Inicio } />
+                <Route exact path='/' component = { Inicio } />
+                <Route path='/Equipo' component = { Equipo } />
             </Switch>
         </BrowserRouter>
     )
